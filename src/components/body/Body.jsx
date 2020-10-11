6bed53b8e7621b41a3716506abebecece7dd303f
yuuki-katsuta <reptpr0314@gmail.com>
2020-10-11 21:23:49 +0900

    ビデオ表示機能実装

---
+++ src/components/body/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Body = (props) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        {props.children}
+      </div>
+    </div>
+
+  )
+}
+
+export default Body
